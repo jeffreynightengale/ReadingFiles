3eefbee17f67e5d6a1df64e3b4e16b86aaf7190d
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-03-18 17:24:24 -0500

    started

---
+++ ReadingInFiles/ReadingInFiles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadingInFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
